5eba3a9f7371b0d845312a4c244485a501cb3417
Ademar Castro <ademar.castro.fh@gmail.com>
2025-05-11 18:58:22 -0400

    [REFACTOR] - Atualiza o seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,12 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import { seedUsers } from './seeders/seedUsers';
 
 const prisma = new PrismaClient();
 
 async function main() {
   console.log('🌱 Iniciando seeding...');
-
-  await seedUsers();
 
   console.log('✅ Seeding concluído.');
 }
