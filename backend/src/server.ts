9568a2cc1620da461b1e43018c8664907556e3f7
Lucas Eduardo <128639233+LucasdeWardo2049@users.noreply.github.com>
2025-05-11 20:32:16 -0400

    [FEAT] - Implementa CRUD básico de tarefas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import tarefaRoutes from './routes/tarefa.routes';
 
 const app = express();
 app.use(express.json());
-app.use('/api', (req, res) => {
-  res.json({ message: 'Hello World!' });
-});
+
+app.use('/api/tarefas', tarefaRoutes);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`🚀 Server em http://localhost:${port}/api`));
